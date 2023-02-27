42d6ac4cfc9f8a1e6961476970e8c7dd06c50300
Daria Hannochenko <soloha7965@gmail.com>
2023-02-27 14:29:43 +0200

    Add view index & template for it

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Tag, Task
+
+
+def index(request):
+    num_tags = Tag.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num tags": num_tags, "num_tasks": num_tasks,
+    }
+    return render(request, "catalog/index.html", context=context)
