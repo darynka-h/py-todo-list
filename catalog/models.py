ef4d665b26d5da112b4d32d2b9d6eee2058b07e9
Daria Hannochenko <soloha7965@gmail.com>
2023-02-27 14:28:46 +0200

    Add models for project

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Executor(AbstractUser):
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    time_creation = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    done = models.BooleanField(default=False)
+    task_tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.content}({self.task_tag})"
