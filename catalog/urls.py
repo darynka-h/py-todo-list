78b71e4e71452bbe7f3834fd754b24f71fd8c9e9
Daria Hannochenko <soloha7965@gmail.com>
2023-02-27 20:19:09 +0200

    Add TagCreateView & TaskCreateView and forms for them

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from catalog.views import index, TagListView, TaskListView
+from catalog.views import index,  TagListView, TaskListView, TagCreateView, TaskCreateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -10,9 +10,19 @@ urlpatterns = [
         name="tag-list",
     ),
     path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create",
+    ),
+    path(
         "tasks/",
         TaskListView.as_view(),
         name="task-list"
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create",
     ),
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from catalog.models import Tag, Task
 
 
 def index(request):
-    num_tags = Tag.objects.count()
-    num_tasks = Task.objects.count()
-
-    context = {
-        "num tags": num_tags, "num_tasks": num_tasks,
-    }
-    return render(request, "catalog/index.html", context=context)
+    # num_tags = Tag.objects.count()
+    # num_tasks = Task.objects.count()
+    #
+    # context = {
+    #     "num tags": num_tags, "num_tasks": num_tasks,
+    # }
+    return render(request, "catalog/index.html")
 
 
 class TagListView(generic.ListView):
     model = Tag
-    context_object_name = "tag_list"
+    # context_object_name = "tag_list"
     template_name = "catalog/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:tag-list")
+
+
 
 
 class TaskListView(generic.ListView):
     model = Task
     context_object_name = "task_list"
-    template_name = "catalog/task_list.html"
+    template_name = "catalog/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:task-list")
